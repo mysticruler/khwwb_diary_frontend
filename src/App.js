9b5901f3a24290053d5ce4f95aca83dd7e071a39
MilanKoshy24 <milanmkoshy@gmail.com>
2024-10-23 18:10:16 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Homepage from './Homepage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Homepage />} />
         
       </Routes>
     </BrowserRouter>
