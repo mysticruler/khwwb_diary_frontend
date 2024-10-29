423a289d82fd8c5a5d0f70271452521ee4dfbd19
MilanKoshy24 <milanmkoshy@gmail.com>
2024-10-29 16:54:20 +0530

    ok

--- src/Homepage.js
+++ src/Homepage.js
@@ -26,10 +26,22 @@ function Homepage() {
 
     return (
         <div className="content-overlay">
+
+            <a
+                href="https://www.instagram.com/m._universe" // Replace with your Instagram link
+                target="_blank" // Open in a new tab
+                rel="noopener noreferrer" // Security best practices
+            >
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/insta.png`}
+                    alt="Instagram"
+                    className="custom-image"
+                />
+            </a>
+
             <h1>Bro Felt bored so he made a website <br></br>Hello goooooois</h1>
             <form onSubmit={handleSubmit} className="user-form">
                 <label>
-                  
                     <input
                         type="text"
                         value={username}
